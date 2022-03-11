31ed4179b83ddd6c51c145379d350a05eb05bb9e
richjp <richard_inada@hotmail.com>
2022-03-10 22:28:40 -0300

    criando banco de dados local

---
+++ database/migrations/2022_03_11_011013_create_canecas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('canecas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id')->unique();
+            $table->string('nome');
+            $table->string('cor');
+            $table->integer('quantidade');
+            $table->float('valor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('canecas');
+    }
+};
